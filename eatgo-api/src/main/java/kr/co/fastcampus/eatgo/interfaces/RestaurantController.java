96edc1bb6a451dae01fe596222383e71e7e6cbac
yujin song <uzini_@likelion.org>
2020-02-04 23:37:47 +0900

    08. 가게 상세 - 1

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -2,6 +2,7 @@ package kr.co.fastcampus.eatgo.interfaces;
 
 import kr.co.fastcampus.eatgo.domain.Restaurant;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -18,5 +19,22 @@ public class RestaurantController {
 		restaurants.add(restaurant);
 
 		return restaurants;
+	}
+
+	@GetMapping("/restaurants/{id}")
+	public Restaurant detail(@PathVariable("id") Long id) {
+		List<Restaurant> restaurants = new ArrayList<>();
+
+		restaurants.add(new Restaurant(20L, "Bob zip", "Seoul"));
+		restaurants.add(new Restaurant(1004L, "Cyber Food", "Seoul"));
+
+		// Java8 추가. filter 기능
+		Restaurant restaurant = restaurants.stream()
+				.filter(r -> r.getId().equals(id))
+				.findFirst()
+				.get();
+		//      .orElse(null); // 없는 경우 테스트할 때 사용. 없는 경우 null.
+
+		return restaurant;
 	}
 }
